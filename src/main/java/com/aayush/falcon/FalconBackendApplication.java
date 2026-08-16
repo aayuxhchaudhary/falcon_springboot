bd5a83ba1159d238c415eef854d8aeb15eb5395b
Aayush <aayushc951@gmail.com>
2026-08-16 11:07:40 +0530

    init

---
+++ src/main/java/com/aayush/falcon/FalconBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aayush.falcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalconBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalconBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aayush/falcon/FalconBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aayush.falcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalconBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
